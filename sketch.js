c546548660d7c0a7902a0c700e435963004bb7dd
shinju2u <82152221+shinju2u@users.noreply.github.com>
2021-05-15 16:30:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -82,18 +82,20 @@ function draw() {
 }
 
 function keyPressed() {
-
-	if(keyCode === RIGHT_ARROW){
-		helicopterSprite.x = helicopterSprite.x + 20;
-		packageBody.position.x  = packageBody.position.x  + 20;
+	if (keyCode === LEFT_ARROW) {
+  
+	  helicopterSprite.x=helicopterSprite.x-20;    
+	  translation={x:-20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+  
+  
+	} else if (keyCode === RIGHT_ARROW) {
+	  helicopterSprite.x=helicopterSprite.x+20;
+	  translation={x:20,y:0}
+	  Matter.Body.translate(packageBody, translation)
 	}
-	
-        if(keyCode === LEFT_ARROW){
-			helicopterSprite.x = helicopterSprite.x - 20;
-			packageBody.position.x= packageBody.position.x  - 20;
+	else if (keyCode === DOWN_ARROW) {
+	  Matter.Body.setStatic(packageBody,false);
+	  
 	}
-
-	if (keyCode === DOWN_ARROW) {
-		Matter.Body.setStatic(packageBody,false); 
-	}
-}
+  }
